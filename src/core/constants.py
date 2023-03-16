992a2cdbf50b8c1e504775cddbfb42c40569d910
jchia <julianigchia@gmail.com>
2023-03-15 22:22:58 -0300

    [feat] Reorganizes constants to access config

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,1 +1,8 @@
+from omegaconf import OmegaConf
+
+# paths
 PREPROCESSING_CONFIG_PATH = "src/configs/preprocessing_config.json"
+
+
+# models from omegaconf
+PREPROCESSING_CONFIG = OmegaConf.load(PREPROCESSING_CONFIG_PATH)
--- src/core/models/config_models.py
+++
@@ -1,6 +0,0 @@
-from omegaconf import OmegaConf
-from src.core import constants
-
-
-preprocessing_conf = OmegaConf.load(constants.PREPROCESSING_CONFIG_PATH)
-    
