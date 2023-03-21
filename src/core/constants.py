d0912edb0cc0b2bf81819d689ff985750dfa5a62
jchia <julianigchia@gmail.com>
2023-03-20 23:18:50 -0300

    [feat] Reorganized general proyect constants

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,8 +1,14 @@
 from omegaconf import OmegaConf
 
-# paths
+
+# config paths
 PREPROCESSING_CONFIG_PATH = "src/configs/preprocessing_config.json"
-COUNT_VECTORIZER_DEFAULT_PATH = "src/core/models/count_vectorizer"
+
+
+# data paths
+COUNT_VECTORIZER_MODEL_DEFAULT_PATH = "src/core/models/count_vectorizer"
+COUNT_VECTORIZER_VOCAB_DEFAULT_PATH = "src/core/data/corpus/count_vectorizer"
+
 
 # models from omegaconf
 PREPROCESSING_CONFIG = OmegaConf.load(PREPROCESSING_CONFIG_PATH)
