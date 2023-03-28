e32e36dd8dccf200c98be80360d8a6d52b4876e5
jchia <julianigchia@gmail.com>
2023-03-28 10:43:30 -0300

    [feat] Updated example

--- src/__main__.py
+++ src/__main__.py
@@ -2,12 +2,16 @@ from src.core.constants import PREPROCESSING_CONFIG
 from src.core.pipeline import Pipeline
 
 
+# CORREGIR TODOS LOS NOMBRES DE LOS ARCHIVOS Y REORDENARLOS
+# VER SI EL VECTORIZADOR PUEDE VECTORIZAR DESDE UN STR
+# AGRENA DESDE LA PIPELINE UNA FORMA DE EJECUTAR TODOS LOS PROCESS
+
 def main() -> None:
     
-    preprocessor_1 = Pipeline(
+    pipe_1 = Pipeline(
         pipeline_conf=PREPROCESSING_CONFIG
     )
-        
+          
     corpus = [
         "HOLA!!1111 gente lindaaaaa!!!",
         "el nombre essssss @Pedro re loco jjajajajjja",
@@ -15,8 +19,12 @@ def main() -> None:
         "su página es www.pedrito.com ...."
     ]
     
+    normalized_corpus = []
     for sent in corpus:
-        print(preprocessor_1.regex_normalization.normalize_text(sent))
+        norm_sent = pipe_1.regex_normalization.normalize_text(sent)
+        normalized_corpus.append(norm_sent)
+        
+    pipe_1.featurization.train(normalized_corpus)
 
 if __name__ == "__main__":
     main()
