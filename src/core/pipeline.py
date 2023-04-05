2784a6162b27f9d85c1d7de3e2bab46fa2d7432d
jchia <julianigchia@gmail.com>
2023-04-05 12:47:24 -0300

    [feat] The 'get_pipe' method was created. Now the constructor doesn't need to expose the created object

--- src/core/pipeline.py
+++ src/core/pipeline.py
@@ -15,11 +15,6 @@ class Pipeline:
     ) -> None:
         self._pipeline_conf = pipeline_conf
         self._pipeline_process = pipeline_process
-        self.__init_pipe()
-    
-    def __init_pipe(self) -> Pipeline:
-        self._set_pipeline_processes()
-        return self
     
     def _set_pipeline_processes(self) -> None:
         for alias, process in self._pipeline_process.items():
@@ -28,7 +23,11 @@ class Pipeline:
                     alias=alias,
                     configs=self._pipeline_conf
                 )
-                    
+    
+    def get_pipe(self) -> Pipeline:
+        self._set_pipeline_processes()
+        return self
+        
     def get_processes_order(self) -> List:
         processes_order = list(self._pipeline_process.keys())
         return processes_order
