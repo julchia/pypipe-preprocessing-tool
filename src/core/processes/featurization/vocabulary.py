54ab3c6cf3e26fd2a5d2c8d971be940cbb9e580c
jchia <julianigchia@gmail.com>
2023-04-11 11:49:09 -0300

    [feat] Vocabalury class was updated to process unique sentences

--- src/core/processes/featurization/vocabulary.py
+++ src/core/processes/featurization/vocabulary.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Iterable, Generator
 
-from src.core.processes.normalization.norm_utils import punctuaction_handler
+from src.core.processes.normalization.norm_utils import punctuaction_handler, lowercase_diacritic_handler
 
 
 class Vocabulary:
@@ -10,32 +10,42 @@ class Vocabulary:
     def __init__(
         self, 
         corpus: List[str],
-        token2idx: Dict[str, int] = None, 
+        corpus2sent: bool = False,
+        text2idx: Dict[str, int] = None, 
         add_unk: bool = True, 
-        unk_token: str = "<<UNK>>",
+        unk_text: str = "<<UNK>>",
+        diacritic: bool = False,
         lower_case: bool = False,
-        norm_punct: bool = False,
+        norm_punct: bool = False
         ) -> None:
         """
+        If 'corpus2sent' is False: variable 'text' is equivalent to 
+        one unique token.
+        If 'corpus2sent' is True: variable 'text' is equivalent to 
+        entire unique sentence.
+        
         """        
+        self._corpus2sent = corpus2sent
+        
         self._norm_punct = norm_punct
         self._lower_case = lower_case
+        self._diacritic = diacritic
         
-        if token2idx is None:
-            token2idx = {}
-        self._token2idx = token2idx
+        if text2idx is None:
+            text2idx = {}
+        self._text2idx = text2idx
 
-        self._idx2token = {
-            idx: token
-            for token, idx in self._token2idx.items()
+        self._idx2text = {
+            idx: text
+            for text, idx in self._text2idx.items()
         }
         
         self._add_unk = add_unk
-        self._unk_token = unk_token
+        self._unk_text = unk_text
         
         self.unk_idx = -1
         if add_unk:
-            self.unk_idx = self.add_token(unk_token)
+            self.unk_idx = self.add_text(unk_text)
             
         self.__init_vocab_from_iterable(self.__iter_corpus(corpus))
     
@@ -43,48 +53,52 @@ class Vocabulary:
         return f"<Vocabulary object (size={len(self)})>"
     
     def __len__(self) -> int:
-        return len(self._token2idx)
+        return len(self._text2idx)
     
     def __iter__(self) -> Generator:
-        for token in self._token2idx.keys():
-            yield token
+        for text in self._text2idx.keys():
+            yield text
     
     def __iter_corpus(self, corpus: List[str]) -> Generator:
         for sent in corpus:
-            for word in sent.split(" "):
-                yield word
+            if self._corpus2sent:
+                yield sent
+            else:
+                for word in sent.split(" "):
+                    yield word
                 
     def __init_vocab_from_iterable(self, iterable: Iterable[List[str]]) -> None:
-        for token in iterable:
-            self.add_token(token)
+        for text in iterable:
+            self.add_text(text)
     
-    def add_token(self, token: str) -> None:
+    def add_text(self, text: str) -> None:
         """
         """
-        if self._lower_case:
-            token = token.lower()
+        if self._lower_case or self._diacritic:
+            text = lowercase_diacritic_handler(text=text)
         
         if self._norm_punct:
-            token = punctuaction_handler(text=token, repl="")
+            text = punctuaction_handler(text=text, repl="")
 
-        if token in self._token2idx:
-            idx = self._token2idx[token]
+        if text in self._text2idx:
+            idx = self._text2idx[text]
         else:
-            idx = len(self._token2idx)
-            self._token2idx[token] = idx
-            self._idx2token[idx] = token
+            idx = len(self._text2idx)
+            self._text2idx[text] = idx
+            self._idx2text[idx] = text
     
-    def get_idx_by_token(self, token: str) -> int:
+    def get_idx_by_text(self, text: str) -> int:
         """
         """
         if self._add_unk:
-            return self._token2idx.get(token, self.unk_idx)
+            return self._text2idx.get(text, self.unk_idx)
         else:
-            return self._token2idx[token]
+            return self._text2idx[text]
         
-    def get_token_by_index(self, index: int) -> str:
+    def get_text_by_index(self, index: int) -> str:
         """
         """
-        if index not in self._idx2token:
+        if index not in self._idx2text:
             raise KeyError(f"The index {index} is not in the Vocabulary")
-        return self._idx2token[index]
+        return self._idx2text[index]
+    
