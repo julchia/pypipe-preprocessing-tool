1b82c446fd33d0b48ae217da9fcdd4884e4083f8
jchia <julianigchia@gmail.com>
2023-04-18 12:43:04 -0300

    [fix] Featurizer names was fixed

--- src/core/constants.py
+++ src/core/constants.py
@@ -2,7 +2,7 @@ from omegaconf import OmegaConf
 
 from src.core import paths
 from src.core.processes.normalization.regex_normalizer import RegexNormalizer
-from src.core.processes.featurization.count_vect_featurizer import SklearnCountVectorizer
+from src.core.processes.featurization.countvec_featurizer import SklearnCountVectorizer
 
 
 PREPROCESSING_CONFIG = OmegaConf.load(paths.PREPROCESSING_CONFIG_PATH)
@@ -10,6 +10,6 @@ PREPROCESSING_CONFIG = OmegaConf.load(paths.PREPROCESSING_CONFIG_PATH)
 
 PIPELINE_PROCESS_ALIAS = {
     "regex_normalization": RegexNormalizer,
-    "count_vect_featurizer": SklearnCountVectorizer
+    "countvec_featurizer": SklearnCountVectorizer
 }
 
--- src/core/paths.py
+++ src/core/paths.py
@@ -2,12 +2,12 @@ PREPROCESSING_CONFIG_PATH = "src/configs/preprocessing_config.json"
 
 
 MODEL_DEFAULT_PATHS = {
-    "count_vect_featurizer": "src/data/models/count_vectorizer",
+    "countvec_featurizer": "src/data/models/count_vectorizer",
     "word2vec_featurizer": "src/data/models/word2vec"
 }
 
 
 VOCAB_DEFAULT_PATHS = {
-    "count_vect_featurizer": "src/data/models/word2vec",
+    "countvec_featurizer": "src/data/models/word2vec",
     "word2vec_featurizer": "src/data/corpus/word2vec"
 }
