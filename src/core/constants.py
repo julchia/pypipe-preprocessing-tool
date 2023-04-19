c8ba75b5b62f38cd3495c4a260f1a7b389907d9f
jchia <julianigchia@gmail.com>
2023-04-18 23:11:20 -0300

    [feat] Word2VecFeaturizer was added to pipeline workflow

--- src/core/constants.py
+++ src/core/constants.py
@@ -3,6 +3,7 @@ from omegaconf import OmegaConf
 from src.core import paths
 from src.core.processes.normalization.regex_normalizer import RegexNormalizer
 from src.core.processes.featurization.countvec_featurizer import CountVecFeaturizer
+from src.core.processes.featurization.word2vec_featurizer import Word2VecFeaturizer
 
 
 PREPROCESSING_CONFIG = OmegaConf.load(paths.PREPROCESSING_CONFIG_PATH)
@@ -10,6 +11,7 @@ PREPROCESSING_CONFIG = OmegaConf.load(paths.PREPROCESSING_CONFIG_PATH)
 
 PIPELINE_PROCESS_ALIAS = {
     "regex_normalization": RegexNormalizer,
-    "countvec_featurizer": CountVecFeaturizer
+    "countvec_featurizer": CountVecFeaturizer,
+    "word2vec_featurizer": Word2VecFeaturizer
 }
 
