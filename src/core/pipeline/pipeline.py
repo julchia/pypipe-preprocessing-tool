a142238d81c2077a4a1b4294ab5cebf77ee1c733
jchia <julianigchia@gmail.com>
2023-04-25 18:10:57 -0300

    [feat] Better handling of methods and loggin messages

--- src/core/pipeline/pipeline.py
+++ src/core/pipeline/pipeline.py
@@ -1,11 +1,15 @@
 from __future__ import annotations
 from typing import List, Dict, Any, Optional, Union, Callable, Iterable
 
+import logging
 from omegaconf import OmegaConf
 
 from src.core import constants
 from src.core.management.managers import CorpusLazyManager
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class Pipeline:
     """
@@ -19,6 +23,7 @@ class Pipeline:
         self._pipeline_conf = pipeline_conf
         self._pipeline_process = pipeline_process
         self._corpus_iterator = corpus_iterator
+        self._pipiline_was_created: bool = False
     
     def _set_pipeline_processes(self) -> None:
         """
@@ -31,18 +36,13 @@ class Pipeline:
                         alias=alias,
                         configs=self._pipeline_conf
                     )
-                
-    def create_pipeline(self) -> Pipeline:
-        """
-        """
-        self._set_pipeline_processes()
-        return self
-    
-    def process_corpus_sequentially(self, corpus: Union[List[str], str]) -> Optional[Any]:
-        """
-        """
-        processed_corpus = self._corpus_iterator(corpus)
+            self._pipiline_was_created = True
         
+    def _get_processes_sequentially(
+        self, 
+        corpus: Union[List[str], str]
+    ) -> Optional[Any]:
+        processed_corpus = self._corpus_iterator(corpus)
         for alias, spec in self._pipeline_process.items():
             if alias in self.__dict__:
                 handler, _ = spec
@@ -50,6 +50,25 @@ class Pipeline:
                     processor=self.__dict__[alias]
                 )
                 processed_corpus = active_handler.process(processed_corpus)
-    
         return processed_corpus
+           
+    def create_pipeline(self) -> Pipeline:
+        """
+        """
+        self._set_pipeline_processes()
+        return self
+    
+    def process_corpus_sequentially(
+        self, 
+        corpus: Union[List[str], str]
+    ) -> Optional[Any]:
+        """
+        """
+        if not self._pipiline_was_created:
+            logger.info(
+                "To run processes sequentially, it is first necessary "
+                "to create a pipeline by calling 'create_pipeline()'"
+            )
+        else:
+            return self._get_processes_sequentially(corpus=corpus)
     
