ed1d524568eb3948f56e2cb6deb805ac7a06533b
jchia <julianigchia@gmail.com>
2023-05-18 17:52:21 -0300

    [feat] Paramater corpus changed to data

--- pypipe/__main__.py
+++ pypipe/__main__.py
@@ -34,7 +34,7 @@ def main():
     """    
     parser = argparse.ArgumentParser(description="Pipeline CLI")
     parser.add_argument("config",help="Alias of configuration file or path to configuration file")
-    parser.add_argument("--data", "-d", help="Path to data file")
+    parser.add_argument("--data", "-d", default=None, help="Path to data file")
     parser.add_argument("--store", "-s", action="store_true", help="Persist output of each process")
     parser.add_argument("--process", "-p", help="Alias of the pipeline process to run")
     parser.add_argument("--method", "-m", required="--process" in sys.argv, help="Method to execute for the specified pipeline process")
@@ -44,7 +44,10 @@ def main():
     
     if args.process:
         process = pipeline.create_pipeline_process(args.process)
-        getattr(process, args.method)(data=pipeline._data, persist=args.store)
+        try:
+            getattr(process, args.method)(data=args.data, persist=args.store)
+        except TypeError:
+            getattr(process, args.method)(data=args.data)
     else:
         pipeline.run_processes_sequentially(persist=args.store)
 
