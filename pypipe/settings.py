81dde18037aa0f159888746913d042ca91830580
jchia <julianigchia@gmail.com>
2023-05-19 18:32:04 -0300

    [feat] Alias constants were modified

--- pypipe/settings.py
+++ pypipe/settings.py
@@ -2,24 +2,13 @@
 
 PREPRO1_CONFIG_PATH = "configs/prepro1.yml"
 
-###### Paths to model resources ######
-
-MODEL_DEFAULT_PATHS = {
-    "countvec": "pypipe/data/models/countvec",
-    "word2vec": "pypipe/data/models/word2vec",
-    "regex_norm": "pypipe/data/models/regex_norm"
-}
-
-VOCAB_DEFAULT_PATHS = {
-    "countvec": "pypipe/data/corpus/countvec",
-    "word2vec": "pypipe/data/corpus/word2vec"
-}
 
 ###### Config alias ######
 
 CONFIG_ALIAS = {
     "prepro1": PREPRO1_CONFIG_PATH
 }
+
 
 ###### Process alias ######
 
@@ -31,5 +20,16 @@ COUNTVEC_FEATURIZER_ALIAS = "countvec"
 WORD2VEC_FEATURIZER_ALIAS = "word2vec"
 
 
+###### Paths to model resources ######
 
+MODEL_DEFAULT_PATHS = {
+    COUNTVEC_FEATURIZER_ALIAS: "pypipe/data/models/countvec",
+    WORD2VEC_FEATURIZER_ALIAS: "pypipe/data/models/word2vec",
+    REGEX_NORMALIZER_ALIAS: "pypipe/data/models/regex_norm"
+}
+
+VOCAB_DEFAULT_PATHS = {
+    COUNTVEC_FEATURIZER_ALIAS: "pypipe/data/corpus/countvec",
+    WORD2VEC_FEATURIZER_ALIAS: "pypipe/data/corpus/word2vec"
+}
 
