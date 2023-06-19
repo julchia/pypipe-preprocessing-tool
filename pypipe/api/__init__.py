377452c6e265034849d0010a4b955340b1c4c175
jchia <julianigchia@gmail.com>
2023-06-19 16:07:32 -0300

    [doc] Doc added

--- pypipe/api/__init__.py
+++ pypipe/api/__init__.py
@@ -9,6 +9,22 @@ flask_bcrypt = Bcrypt()
 
 def create_app(config_name: str) -> Flask:  
     """
+    Create and configure the Flask application.
+
+    Parameters:
+    - config_name (str): The name of the configuration to use for the 
+        application.
+
+    Returns:
+    - Flask: The Flask application object.
+
+    Description:
+    This function creates a new Flask application object and configures it 
+    based on the specified 'config_name'. It loads the configuration settings 
+    from the 'config.py' module.
+
+    Example usage:
+    >> app = create_app('development')
     """
     app = Flask(__name__)
     app.config.from_object(config.get_config_by_name[config_name])
